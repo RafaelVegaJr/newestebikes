99d0d299f49095283d471f335c02b87729f9148a
RafaelVegaJr <titovega12@gmail.com>
2023-10-21 08:53:28 -0700

    new new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-// import SignUp from "./components/pages/SignUp";
-// import VideoPlayer from "./components/VideoPlayer";
-// import Services from "./components/pages/Services";
+import SignUp from "./pages/SignUp";
+import VideoPlayer from "./components/VideoPlayer";
+import Services from "./pages/Services";
 // import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App";
+import "./App.css";
 import Home from "./pages/Home";
 // import ProtectedRoute from "./path-to-your-protected-route-component";
 
 const App = () => {
   <h1>Hello</h1>;
   console.log("Are you working");
-  // <Auth0Provider
-  // domain="dev-hgmqfdn6h1uohhld.us.auth0.com"
-  // clientId="bncvPbYM66lwG06SQrII1dp8hqH6QewP"
-  // redirectUri={window.location.origin}
-  // >
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/sign-up" element={<SignUp />} /> */}
-      {/* <Route path="/services" element={<Services />} /> */}
-      {/* <Route path="/watch-trailer" element={<VideoPlayer />} /> */}
-    </Routes>
-  </Router>;
-  // </Auth0Provider>;
+  return (
+    // <Auth0Provider
+    // domain="dev-hgmqfdn6h1uohhld.us.auth0.com"
+    // clientId="bncvPbYM66lwG06SQrII1dp8hqH6QewP"
+    // redirectUri={window.location.origin}
+    // >
+
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/watch-trailer" element={<VideoPlayer />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
+    // </Auth0Provider>;
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// Create a root container where the app will be rendered
+const root = createRoot(document.getElementById("root"));
+// Use the new createRoot API to render the app
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
