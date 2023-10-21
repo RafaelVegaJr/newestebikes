f3c0fafbc3710d04e175745d9a6f1420ff6953ec
RafaelVegaJr <titovega12@gmail.com>
2023-10-21 16:13:23 -0700

    new shidfgft

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,11 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <ul
+            className={
+              click || window.innerWidth > 960 ? "nav-menu active" : "nav-menu"
+            }
+          >
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
